77a4a53c5b78e74bf26b6eb2c13678c43dec3973
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-03-26 11:59:19 +0000

    カレンダー

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -3,10 +3,10 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
-
   var calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth'
+    plugins: [ dayGridPlugin ],
+    initialView: 'dayGridMonth',
+    events: eventData // eventData変数を使用してイベントデータを渡す
   });
 
   calendar.render();
