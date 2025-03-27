d19f6e939c8e66ded9e95391abf83da5f7826dac
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-03-27 00:05:53 +0000

    add

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,12 +1,22 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from "@fullcalendar/list"; //この行を追加
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ],
+    plugins: [dayGridPlugin, listPlugin],　//listPluginを追加
     initialView: 'dayGridMonth',
-    events: '/events'
+    locale: "jp", //この行を追加
+    events: '/events', 
+    //レスポンシブ処理を追加
+    windowResize: function () { 
+        if (window.innerWidth < 991.98) {
+            calendar.changeView('listMonth');
+        } else {
+            calendar.changeView('dayGridMonth');
+        }
+    },
   });
 
   calendar.render();
